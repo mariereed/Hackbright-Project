a8f2816c59b6a23bbd884e032c6bf36a973d870e
vagrant <vagrant@vagrant.vm>
2017-11-07 20:01:54 +0000

    Fixed What two wear. content:encoded still not seeding

--- seed_data.py
+++ seed_data.py
@@ -17,10 +17,10 @@ def my_function():
     blog_rss_urls = ["http://feeds.feedburner.com/MrMoneyMustache?format=xml",
                      "http://feeds.feedburner.com/StudyHacks?format=xml",
                      "http://feeds2.feedburner.com/PsychologyBlog?format=xml",
-                     "https://www.desmogblog.com/rss.xml"
+                     "https://www.desmogblog.com/rss.xml",
+                     "http://feeds.feedburner.com/readytwowear?format=xml"
                      ]
 
-# "http://feeds.feedburner.com/readytwowear?format=xml"
     # Clean out the tables!
     Blog.query.delete()
     Article.query.delete()
@@ -31,11 +31,7 @@ def my_function():
         response = requests.get(blog)
 
         # Create the namespace
-        namespace = dict([
-            node for _, node in ET.iterparse(
-                StringIO(response.text), events=['start-ns']
-            )
-        ])
+        namespace = dict([node for _, node in ET.iterparse(StringIO(response.text), events=['start-ns'])])
 
         # Parse as element
         root = ET.fromstring(response.text)
@@ -44,52 +40,111 @@ def my_function():
         I now have to account for differences in the xml files.
         Such as missing information, strange element tags."""
 
-        # Define all fields for Blog
-        build_date = root.find('.//lastBuildDate').text
-        name = root.find('.//title').text
-        rss_url = root.find('.//link').text
+        # If not what two wear:
+        if root.find('.//'):
+            # All same for name and link
+            name = root.find('.//title').text
+            rss_url = root.find('.//link').text
 
-        # Insantiate the blog
-        # ****** MY ERROR IS HERE********
-        blog = Blog(name=name, rss_url=rss_url, build_date=build_date)
+            if root.find('.//lastBuildDate'):
+                build_date = root.find('.//lastBuildDate').text
+                # if what 2 wear.. build_date = ?
+                blog = Blog(name=name, rss_url=rss_url, build_date=build_date)
+            else:
+                blog = Blog(name=name, rss_url=rss_url)
 
-        # Add the blog to the session and commit it
-        db.session.add(blog)
-        db.session.commit()
+            # Add the blog to the session and commit it
+            db.session.add(blog)
+            db.session.commit()
 
-        # Now look for the first article for that blog in the RSS feed
-        # Check later to make sure the first is actually the newest
-        # Doesn't matter for first one, though.
-        items = root.findall('.//item')
-        first_article = items[0]
+            # Now look for the first article for that blog in the RSS feed
+            # Check later to make sure the first is actually the newest
+            # Doesn't matter for first one, though.
 
-        # Gather the needed data, instantiate and commit
-        publish_date = first_article.find('.//pubDate').text
-        title = first_article.find('.//title').text
-        url = first_article.find('.//link').text
-        description = first_article.find('.//description').text
-        # Might not have content...
+            items = root.findall('.//item')
+            first_article = items[0]
 
-        content_obj = first_article.find('content:encoded', namespace)
+            # Gather the needed data, instantiate and commit
+            publish_date = first_article.find('.//pubDate').text
+            title = first_article.find('.//title').text
+            url = first_article.find('.//link').text
+            description = first_article.find('.//description').text
+            # Might not have content...
 
-        article = title
+            # Works this time because none others have content
+            content_obj = first_article.find('content:encoded', namespace)
 
-        # ADD CONTENT!
-        if content_obj:
-            content = content_obj.text()
-            article = Article(blog_id=blog.blog_id, title=title, activity=True,
-                              url=url, description=description, content=content,
-                              publish_date=publish_date)
+            article = title
+
+            # ADD CONTENT!
+            if content_obj:
+                content = content_obj.text()
+                article = Article(blog_id=blog.blog_id, title=title, activity=True,
+                                  url=url, description=description, content=content,
+                                  publish_date=publish_date)
+            else:
+                article = Article(blog_id=blog.blog_id, title=title, activity=True,
+                                  url=url, description=description, publish_date=publish_date)
+
+            db.session.add(article)
+            db.session.commit()
+
+        # Else what two wear
         else:
-            article = Article(blog_id=blog.blog_id, title=title, activity=True,
-                              url=url, description=description, publish_date=publish_date)
+            title = 'title'
+            insert = "{" + "{}".format(namespace['']) + "}"
+            name = root.find(insert + title).text
 
-        db.session.add(article)
-        db.session.commit()
+            # finding the right link tag (make this a func later)
+            link = 'link'
+            insert = "{" + "{}".format(namespace['']) + "}"
+            links = root.findall(insert + link)
+            for link in links:
+                if link.get('rel') == "alternate":
+                    rss_url = link.get('href')
+
+            published = 'updated'
+            insert = "{" + "{}".format(namespace['']) + "}"
+            build_date = root.find(insert + published).text
+
+            blog = Blog(name=name, rss_url=rss_url, build_date=build_date)
+
+            db.session.add(blog)
+            db.session.commit()
+
+            entry = 'entry'
+            insert = "{" + "{}".format(namespace['']) + "}"
+            items = root.findall(insert + entry)
+            first_article = items[0]
+
+            title = 'title'
+            insert = "{" + "{}".format(namespace['']) + "}"
+            title = first_article.find(insert + title).text
+
+            # finding the right link tag (make this a func later)
+            link = 'link'
+            insert = "{" + "{}".format(namespace['']) + "}"
+            links = first_article.findall(insert + link)
+            for link in links:
+                if link.get('rel') == "alternate":
+                    url = link.get('href')
+
+            published = 'published'
+            insert = "{" + "{}".format(namespace['']) + "}"
+            publish_date = first_article.find(insert + published).text
+
+            content = 'content'
+            insert = "{" + "{}".format(namespace['']) + "}"
+            content = first_article.find(insert + content).text
+
+            article = Article(blog_id=blog.blog_id, title=title, activity=True,
+                              url=url, content=content, publish_date=publish_date)
+
+            db.session.add(article)
+            db.session.commit()
 
 
 # ----------------------------------------------------
-
 
 if __name__ == "__main__":
 
