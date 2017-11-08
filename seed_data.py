8c914f7ffe2e2cd8dcc7be9b492713eeb29904f0
vagrant <vagrant@vagrant.vm>
2017-11-08 00:51:52 +0000

    In process refactoring seed_data.py.

--- seed_data.py
+++ seed_data.py
@@ -8,144 +8,225 @@ import requests
 import sys
 from server import app
 
-# Fixes ascii --> unicode error
-reload(sys)
-sys.setdefaultencoding('utf-8')
+
+blog_rss_urls = ["http://feeds.feedburner.com/MrMoneyMustache?format=xml",
+                 "http://feeds.feedburner.com/StudyHacks?format=xml",
+                 "http://feeds2.feedburner.com/PsychologyBlog?format=xml",
+                 "https://www.desmogblog.com/rss.xml",
+                 "http://feeds.feedburner.com/readytwowear?format=xml"
+                 ]
 
 
-def my_function():
-    blog_rss_urls = ["http://feeds.feedburner.com/MrMoneyMustache?format=xml",
-                     "http://feeds.feedburner.com/StudyHacks?format=xml",
-                     "http://feeds2.feedburner.com/PsychologyBlog?format=xml",
-                     "https://www.desmogblog.com/rss.xml",
-                     "http://feeds.feedburner.com/readytwowear?format=xml"
-                     ]
+def prep_for_seed():
+    """All the steps necessary to start the seeding."""
+
+    # Fixes ascii --> unicode error
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
 
     # Clean out the tables!
     Blog.query.delete()
     Article.query.delete()
 
-    # Gets all the necessary information for each blog and instantiates
-    for blog in blog_rss_urls:
-        # Takes a string for blog
-        response = requests.get(blog)
 
-        # Create the namespace
-        namespace = dict(
-            [node for _, node in ET.iterparse(
-                StringIO(response.text), events=['start-ns']
-                )]
-            )
+def get_response(blog):
+    """Sends a request to a blog url, returns the response."""
 
-        # Parse as element
-        root = ET.fromstring(response.text)
+    response = requests.get(blog)
 
-        """ Up to this point everything has been consistent.
-        I now have to account for differences in the xml files.
-        Such as missing information, strange element tags."""
+    return response
 
-        # If not what two wear:
-        if root.find('.//'):
-            # All same for name and link
-            name = root.find('.//title').text
-            rss_url = root.find('.//link').text
 
-            if root.find('.//lastBuildDate') is not None:
-                build_date = root.find('.//lastBuildDate').text
-                # if what 2 wear.. build_date = ?
-                blog = Blog(name=name, rss_url=rss_url, build_date=build_date)
-            else:
-                blog = Blog(name=name, rss_url=rss_url)
+def create_root(response):
+    """Creates and returns the ElementTree root."""
 
-            # Add the blog to the session and commit it
-            db.session.add(blog)
-            db.session.commit()
+    root = ET.fromstring(response.text)
 
-            # Now look for the first article for that blog in the RSS feed
-            # Check later to make sure the first is actually the newest
-            # Doesn't matter for first one, though.
+    return root
 
-            items = root.findall('.//item')
-            first_article = items[0]
 
-            # Gather the needed data, instantiate and commit
-            publish_date = first_article.find('.//pubDate').text
-            title = first_article.find('.//title').text
-            url = first_article.find('.//link').text
-            description = first_article.find('.//description').text
-            # Might not have content...
+def create_namespace(response):
+    """Create the namespace."""
 
-            # Works this time because none others have content
-            content_obj = first_article.find('content:encoded', namespace)
+    namespace = dict(
+        [node for _, node in ET.iterparse(
+            StringIO(response.text), events=['start-ns']
+            )]
+        )
 
-            article = title
+    return namespace
 
-            # ADD CONTENT!
-            if content_obj is not None:
-                content = content_obj.text
-                article = Article(blog_id=blog.blog_id, title=title, activity=True,
-                                  url=url, description=description, content=content,
-                                  publish_date=publish_date)
-            else:
-                article = Article(blog_id=blog.blog_id, title=title, activity=True,
-                                  url=url, description=description, publish_date=publish_date)
 
-            db.session.add(article)
-            db.session.commit()
+def normalize_tag(tag):
+    """When a blog parses oddly and requires special formatting,
+    this is one of the solutions. Returns the formatted root.find.
+    Should be used in another function to find at specific node.
+    """
 
-        # Else what two wear
+    insert = "{" + "{}".format(namespace['']) + "}"
+    search_for = insert + tag
+    return search_for
+
+
+def find_tag(node, tag):
+    """Takes in a node (ie: root) and an element tag, returns the text."""
+    if is_normal():
+        search_for = './/{}'.format(tag)
+    else:
+        search_for = normalize_tag(tag)
+
+    return node.find(search_for)
+
+
+def is_normal():
+    if root.find('.//'):
+        return True
+    else:
+        return False
+
+
+def create_value_dict(node):
+    """Creates a attrib-value dictionary for instantiating each instance object."""
+
+    tag_values = {}
+
+    for attrib in desired_tag:
+        if find_tag(node, desired_tag[attrib]) is not None:
+            tag_values[attrib] = find_tag(node, desired_tag[attrib]).text
         else:
-            title = 'title'
-            insert = "{" + "{}".format(namespace['']) + "}"
-            name = root.find(insert + title).text
+            tag_values[attrib] = None
 
-            # finding the right link tag (make this a func later)
-            link = 'link'
-            insert = "{" + "{}".format(namespace['']) + "}"
-            links = root.findall(insert + link)
-            for link in links:
-                if link.get('rel') == "alternate":
-                    rss_url = link.get('href')
+    return tag_values
 
-            published = 'updated'
-            insert = "{" + "{}".format(namespace['']) + "}"
-            build_date = root.find(insert + published).text
 
-            blog = Blog(name=name, rss_url=rss_url, build_date=build_date)
+def add_and_commit(instance_type):
+    """Adds and commits the instance into the db."""
 
-            db.session.add(blog)
-            db.session.commit()
+    db.session.add(instance_type)
+    db.session.commit()
 
-            entry = 'entry'
-            insert = "{" + "{}".format(namespace['']) + "}"
-            items = root.findall(insert + entry)
-            first_article = items[0]
+    return "Added and committed!"
 
-            title = 'title'
-            insert = "{" + "{}".format(namespace['']) + "}"
-            title = first_article.find(insert + title).text
 
-            # finding the right link tag (make this a func later)
-            link = 'link'
-            insert = "{" + "{}".format(namespace['']) + "}"
-            links = first_article.findall(insert + link)
-            for link in links:
-                if link.get('rel') == "alternate":
-                    url = link.get('href')
+def find_all(node, tag):
+    """Find all of a certain tag from node."""
 
-            published = 'published'
-            insert = "{" + "{}".format(namespace['']) + "}"
-            publish_date = first_article.find(insert + published).text
+    if is_normal():
+        search_for = './/{}'.format(tag)
+    else:
+        search_for = normalize_tag(tag)
 
-            content = 'content'
-            insert = "{" + "{}".format(namespace['']) + "}"
-            content = first_article.find(insert + content).text
+    return node.findall(search_for)
 
-            article = Article(blog_id=blog.blog_id, title=title, activity=True,
-                              url=url, content=content, publish_date=publish_date)
 
-            db.session.add(article)
-            db.session.commit()
+def seed_data():
+    """Actually seed the data. """
+
+    for blog in blog_rss_urls:
+
+        response = get_response(blog)
+        root = create_root(response)
+        namespace = create_namespace(response)
+
+        desired_tag = {'name': 'title', 'rss_url': 'link', 'build_date': 'lastBuildDate'}
+
+        # DOES NOT ACCOUNT FOR WHAT 2 WEAR
+        # How do I have multiple options for build_date? a list?
+        # Maybe I'll just have to look for date like things.
+        create_value_dict(root)
+
+        blog = Blog(name=desired_tag['name'],
+                    rss_url=desired_tag['rss_url'],
+                    build_date=desired_tag['build_date'])
+
+        add_and_commit(blog)
+
+        # Now look for the first article for that blog in the RSS feed
+        # Check later to make sure the first is actually the newest
+        # Doesn't matter for first one, though.
+
+        items = find_all(root, 'item')
+        first_article = items[0]
+
+        desired_tag = {'title': 'title', 'url': 'link', 'publish_date': 'pubDate', 'description': 'description'}
+
+        create_value_dict(first_article)
+
+        article = find_tag(first_article, 'title').text
+
+        article = Article(blog_id=blog.blog_id,
+                          title=desired_tag['title'],
+                          activity=True,
+                          url=desired_tag['url'],
+                          description=desired_tag['description'],
+                          publish_date=desired_tag['publish_date'])
+
+        # content_obj = first_article.find('content:encoded', namespace)
+        # # ADD CONTENT!
+        # if content_obj is not None:
+        #     content = content_obj.text
+        #     article = Article(blog_id=blog.blog_id, title=title, activity=True,
+        #                       url=url, description=description, content=content,
+        #                       publish_date=publish_date)
+        # else:
+        #     article = Article(blog_id=blog.blog_id, title=title, activity=True,
+        #                       url=url, description=description, publish_date=publish_date)
+
+        add_and_commit(article)
+
+        # # Else what two wear
+        # else:
+        #     title = 'title'
+        #     insert = "{" + "{}".format(namespace['']) + "}"
+        #     name = root.find(insert + title).text
+
+        #     # finding the right link tag (make this a func later)
+        #     link = 'link'
+        #     insert = "{" + "{}".format(namespace['']) + "}"
+        #     links = root.findall(insert + link)
+        #     for link in links:
+        #         if link.get('rel') == "alternate":
+        #             rss_url = link.get('href')
+
+        #     published = 'updated'
+        #     insert = "{" + "{}".format(namespace['']) + "}"
+        #     build_date = root.find(insert + published).text
+
+        #     blog = Blog(name=name, rss_url=rss_url, build_date=build_date)
+
+        #     db.session.add(blog)
+        #     db.session.commit()
+
+        #     entry = 'entry'
+        #     insert = "{" + "{}".format(namespace['']) + "}"
+        #     items = root.findall(insert + entry)
+        #     first_article = items[0]
+
+        #     title = 'title'
+        #     insert = "{" + "{}".format(namespace['']) + "}"
+        #     title = first_article.find(insert + title).text
+
+        #     # finding the right link tag (make this a func later)
+        #     link = 'link'
+        #     insert = "{" + "{}".format(namespace['']) + "}"
+        #     links = first_article.findall(insert + link)
+        #     for link in links:
+        #         if link.get('rel') == "alternate":
+        #             url = link.get('href')
+
+        #     published = 'published'
+        #     insert = "{" + "{}".format(namespace['']) + "}"
+        #     publish_date = first_article.find(insert + published).text
+
+        #     content = 'content'
+        #     insert = "{" + "{}".format(namespace['']) + "}"
+        #     content = first_article.find(insert + content).text
+
+        #     article = Article(blog_id=blog.blog_id, title=title, activity=True,
+        #                       url=url, content=content, publish_date=publish_date)
+
+        #     db.session.add(article)
+        #     db.session.commit()
 
 
 # ----------------------------------------------------
@@ -155,5 +236,7 @@ if __name__ == "__main__":
     connect_to_db(app, "postgresql:///projectdb")
 
     # In case tables haven't been created, create them
+    prep_for_seed()
     db.create_all()
-    my_function()
+    seed_data()
+
