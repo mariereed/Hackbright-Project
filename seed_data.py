57717aaacc9848afe5ea27b71063badbc33f3e2b
vagrant <vagrant@vagrant.vm>
2017-11-09 00:11:52 +0000

    Got refactored code to work for all except w2w.

--- seed_data.py
+++ seed_data.py
@@ -9,14 +9,6 @@ import sys
 from server import app
 
 
-blog_rss_urls = ["http://feeds.feedburner.com/MrMoneyMustache?format=xml",
-                 "http://feeds.feedburner.com/StudyHacks?format=xml",
-                 "http://feeds2.feedburner.com/PsychologyBlog?format=xml",
-                 "https://www.desmogblog.com/rss.xml",
-                 "http://feeds.feedburner.com/readytwowear?format=xml"
-                 ]
-
-
 def prep_for_seed():
     """All the steps necessary to start the seeding."""
 
@@ -57,7 +49,7 @@ def create_namespace(response):
     return namespace
 
 
-def normalize_tag(tag):
+def normalize_tag(tag, namespace):
     """When a blog parses oddly and requires special formatting,
     this is one of the solutions. Returns the formatted root.find.
     Should be used in another function to find at specific node.
@@ -68,19 +60,19 @@ def normalize_tag(tag):
     return search_for
 
 
-def find_tag(node, tag):
+def find_tag(node, tag, namespace):
     """Takes in a node (ie: root) and an element tag, returns the text."""
 
     if tag == 'link':
-        links = find_all(node, 'link')
+        links = find_all(node, 'link', namespace)
 
         # If links[0] has no attributes
-        if not links[0].get():
+        if not links[0].get('rel'):
             # Follow the else statement below
-            if is_normal():
+            if is_normal(node):
                 search_for = './/{}'.format(tag)
             else:
-                search_for = normalize_tag(tag)
+                search_for = normalize_tag(tag, namespace)
 
             return node.find(search_for).text
 
@@ -90,50 +82,48 @@ def find_tag(node, tag):
                 return correct_link
 
     else:
-        if is_normal():
+        if is_normal(node):
             search_for = './/{}'.format(tag)
         else:
-            search_for = normalize_tag(tag)
+            search_for = normalize_tag(tag, namespace)
 
         return node.find(search_for)
 
 
-def is_normal():
-    if root.find('.//'):
+def is_normal(node):
+    if node.find('.//') is not None:
         return True
     else:
         return False
 
 
-def create_value_dict(node):
+def create_value_dict(node, desired_tag, namespace):
     """Creates a attrib-value dictionary for instantiating each instance object."""
 
     tag_values = {}
 
     for attrib in desired_tag:
-        if attrib == 'link':
+        if attrib == 'rss_url' or attrib == 'url':
             # Don't want .text because it is already a string
-            tag_values[attrib] = find_tag(node, desired_tag[attrib])
+            tag_values[attrib] = find_tag(node, desired_tag[attrib], namespace)
             # Don't do below, return to loop
-            continue
 
-        if attrib == 'content':
-            content_obj = first_article.find('content:encoded', namespace)
+        elif attrib == 'content':
+            content_obj = node.find('content:encoded', namespace)
             if content_obj is not None:
                 tag_values[attrib] = content_obj.text
 
-
-        if type(desired_tag[attrib]) is 'list':
+        elif type(desired_tag[attrib]) == list:
             for i in range(len(desired_tag[attrib])):
-                if find_tag(node, desired_tag[attrib][i]) is not None:
-                    tag_values[attrib] = find_tag(node, desired_tag[attrib][i]).text
+                if find_tag(node, desired_tag[attrib][i], namespace) is not None:
+                    tag_values[attrib] = find_tag(node, desired_tag[attrib][i], namespace).text
 
             # If for loop completes and still no value for attrib, then set to none
             if not tag_values.get(attrib):
                     tag_values[attrib] = None
         else:
-            if find_tag(node, desired_tag[attrib]) is not None:
-                tag_values[attrib] = find_tag(node, desired_tag[attrib]).text
+            if find_tag(node, desired_tag[attrib], namespace) is not None:
+                tag_values[attrib] = find_tag(node, desired_tag[attrib], namespace).text
             else:
                 tag_values[attrib] = None
 
@@ -149,30 +139,81 @@ def add_and_commit(instance_type):
     return "Added and committed!"
 
 
-def find_all(node, tag):
+def find_all(node, tag, namespace):
     """Find all of a certain tag from node."""
 
-    if is_normal():
+    if is_normal(node):
         search_for = './/{}'.format(tag)
     else:
-        search_for = normalize_tag(tag)
+        search_for = normalize_tag(tag, namespace)
 
     return node.findall(search_for)
 
 
-def find_entry_tag():
+def find_entry_tag(node, namespace):
     """Determines the name of the item or entry in the xml file. Returns the correct tag name."""
 
-    if find_all(root, 'item') is not None:
+    if len(find_all(node, 'item', namespace)) != 0:
         tag = 'item'
-    if find_all(root, 'entry') is not None:
+    if len(find_all(node, 'entry', namespace)) != 0:
         tag = 'entry'
 
     return tag
 
 
+desired_tag = {'name': 'title', 'rss_url': 'link', 'build_date': ['lastBuildDate', 'published'],
+                       'title': 'title', 'url': 'link', 'publish_date': ['pubDate', 'published'],
+                       'description': 'description', 'content': 'content'}
+
+
+def seed_blog(blog, root, namespace):
+
+    tag_values = create_value_dict(root, desired_tag, namespace)
+    print
+    print root
+    print desired_tag
+    print tag_values
+    print namespace
+    print
+    blog = Blog(name=tag_values['name'],
+                rss_url=tag_values['rss_url'],
+                build_date=tag_values['build_date'])
+
+    add_and_commit(blog)
+
+    return blog
+
+
+def seed_article(blog, root, namespace):
+
+    items = find_all(root, find_entry_tag(root, namespace), namespace)
+    print find_entry_tag(root, namespace)
+    print items
+    first_article = items[0]
+
+    tag_values = create_value_dict(first_article, desired_tag, namespace)
+
+    article = find_tag(first_article, 'title', namespace).text
+
+    article = Article(blog_id=blog.blog_id,
+                      title=tag_values['title'],
+                      activity=True,
+                      url=tag_values['url'],
+                      description=tag_values['description'],
+                      publish_date=tag_values['publish_date'])
+
+    add_and_commit(article)
+
+
 def seed_data():
     """Actually seed the data. """
+
+    blog_rss_urls = ["http://feeds.feedburner.com/StudyHacks?format=xml",
+                     "http://feeds2.feedburner.com/PsychologyBlog?format=xml",
+                     "https://www.desmogblog.com/rss.xml"]
+
+    "http://feeds.feedburner.com/MrMoneyMustache?format=xml"
+    "http://feeds.feedburner.com/readytwowear?format=xml"
 
     for blog in blog_rss_urls:
 
@@ -180,34 +221,9 @@ def seed_data():
         root = create_root(response)
         namespace = create_namespace(response)
 
-        # For the blog
-        desired_tag = {'name': 'title', 'rss_url': 'link', 'build_date': ['lastBuildDate', 'published']}
-        tag_values = create_value_dict(root)
+        blog = seed_blog(blog, root, namespace)
 
-        blog = Blog(name=tag_values['name'],
-                    rss_url=tag_values['rss_url'],
-                    build_date=tag_values['build_date'])
-
-        add_and_commit(blog)
-
-        # For the article
-        items = find_all(root, find_entry_tag())
-        first_article = items[0]
-
-        desired_tag = {'title': 'title', 'url': 'link', 'publish_date': ['pubDate', 'published'],
-                       'description': 'description', 'content': 'content'}
-
-        tag_values = create_value_dict(first_article)
-        article = find_tag(first_article, 'title').text
-
-        article = Article(blog_id=blog.blog_id,
-                          title=tag_values['title'],
-                          activity=True,
-                          url=tag_values['url'],
-                          description=tag_values['description'],
-                          publish_date=tag_values['publish_date'])
-
-        add_and_commit(article)
+        seed_article(blog, root, namespace)
 
 
 # ----------------------------------------------------
@@ -217,6 +233,6 @@ if __name__ == "__main__":
     connect_to_db(app, "postgresql:///projectdb")
 
     # In case tables haven't been created, create them
-    prep_for_seed()
     db.create_all()
+    prep_for_seed()
     seed_data()
