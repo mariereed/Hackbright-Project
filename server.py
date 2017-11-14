e85d96d5b33c454f76a528d2287995455e3b9a58
vagrant <vagrant@vagrant.vm>
2017-11-14 00:20:21 +0000

    re-created beautiful soup logic. Created route that dsplays the formatted content of a single Blog.

--- server.py
+++ server.py
@@ -6,9 +6,10 @@ import xml.etree.ElementTree as ET
 import requests
 import sys
 from model import User, Blog, User_blog, Favorite, Article, connect_to_db, db
+from bs import beautify
+# import bs
 
 # -------- Set Up ----------------------------------------------
-
 app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
@@ -16,6 +17,7 @@ app.secret_key = "ABC"
 
 # Raise error for undefined variable
 app.jinja_env.undefined = StrictUndefined
+
 
 # -------- Routes ----------------------------------------------
 
@@ -36,6 +38,38 @@ def display_some_data():
 
     return render_template('data.html', blogs=blogs, articles=articles)
 
+@app.route('/formated_data')
+def display_formated_data():
+    """ The purpose of this page is to test that formatted data displays."""
+
+    articles = Article.query.all()
+    article_1 = articles[0]
+
+    # do stuff to it, then reassign to variables
+    new_content = str(beautify(article_1.content))
+
+
+    return render_template('data2.html', new_content=new_content, article_1=article_1)
+
+@app.route('/return_content.json')
+def return_json():
+
+    articles = Article.query.all()
+    article_1 = articles[3]
+
+    new_string = str(beautify(article_1.content))
+
+    new_content = {'content': new_string}
+
+    return jsonify(new_content)
+
+@app.route('/test')
+def display_it():
+    return render_template('testy.html')
+
+@app.route('/test2')
+def display_it_2():
+    return render_template('testy2.html')
 
 # -------- The following is a route template --------------------
 
