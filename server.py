14352deb3839616f439d1dcf5e6182d7540526fb
vagrant <vagrant@vagrant.vm>
2017-12-01 20:32:45 +0000

    Can't comment that out without removing decorators.

--- server.py
+++ server.py
@@ -49,17 +49,17 @@ def login_required(decorated_view):
     return decorated_function
 
 
-# def nocache(view):
-#     @wraps(view)
-#     def no_cache(*args, **kwargs):
-#         response = make_response(view(*args, **kwargs))
-#         response.headers['Last-Modified'] = datetime.now()
-#         response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
-#         response.headers['Pragma'] = 'no-cache'
-#         response.headers['Expires'] = '-1'
-#         return response
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
 
-    # return update_wrapper(no_cache, view)
+    return update_wrapper(no_cache, view)
 
 
 @app.before_request
